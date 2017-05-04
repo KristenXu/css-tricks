50a8e6e522bad64a65d1bb254dd52e0698f1b8ae
Chainn <578579786@qq.com>
2017-05-04 14:55:26 +0800

    Add hight light for code editor

--- code_editor/loadCodeCounts.js
+++ code_editor/loadCodeCounts.js
@@ -2,9 +2,10 @@ let initCodeLines = function () {
     let codeBlocks = document.getElementsByClassName('has-line-num');
     Array.prototype.forEach.call(codeBlocks, (codeBlock) => {
         codeBlock.innerHTML = codeBlock.textContent
-        .trim()
-        .replace(/^/, '<span class="code-line">')
-        .replace(/\n/g, '</span>\n<span class="code-line">')
-        .replace(/$/, '</span>');
+            .trim()
+            .replace(/^/, '<span class="code-line">')
+            .replace(/\n/g, '</span>\n<span class="code-line">')
+            .replace(/$/, '</span>');
+        hljs.highlightBlock(codeBlock);
     });
 }
